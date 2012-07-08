b7ef424485baa9467289770434219c4fbd2324da
Simon Cropp <simon.cropp@gmail.com>
2012-07-08 15:02:30 +1000

    bump version

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -4,5 +4,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Validar")]
 [assembly: AssemblyProduct("Validar")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("0.5.0.0")]
-[assembly: AssemblyFileVersion("0.5.0.0")]
+[assembly: AssemblyVersion("0.6.0.0")]
+[assembly: AssemblyFileVersion("0.6.0.0")]
