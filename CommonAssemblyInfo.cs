47e35b570b1a7325e5e68d249d1bf3c3cf2c6921
Simon Cropp <simon.cropp@gmail.com>
2012-12-16 14:28:05 +1100

    bump version

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyTitle("Validar")]
 [assembly: AssemblyProduct("Validar")]
-[assembly: AssemblyVersion("0.10.0.0")]
-[assembly: AssemblyFileVersion("0.10.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
