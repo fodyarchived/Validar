1bcfda848c3cf1d9d9dee016fcc3585455b71b63
Simon Cropp <simon.cropp@gmail.com>
2013-06-23 10:25:59 +1000

    same

--- AssemblyToProcess/ValidationTemplate.cs
+++ AssemblyToProcess/ValidationTemplate.cs
@@ -1,28 +1,44 @@
+
 using System;
 using System.Collections;
+using System.Collections.Concurrent;
 using System.ComponentModel;
 using System.Linq;
 using FluentValidation;
 using FluentValidation.Results;
 
-
-public class ValidationTemplate : IDataErrorInfo, INotifyDataErrorInfo
+public class ValidationTemplate :
+    IDataErrorInfo,
+    INotifyDataErrorInfo
 {
     INotifyPropertyChanged target;
     IValidator validator;
     ValidationResult validationResult;
+    static ConcurrentDictionary<RuntimeTypeHandle, IValidator> validators = new ConcurrentDictionary<RuntimeTypeHandle, IValidator>();
 
     public ValidationTemplate(INotifyPropertyChanged target)
     {
         this.target = target;
-        validator = ValidationFactory.GetValidator(target.GetType());
+        validator = GetValidator(target.GetType());
         validationResult = validator.Validate(target);
         target.PropertyChanged += Validate;
     }
 
+    static IValidator GetValidator(Type modelType)
+    {
+        IValidator validator;
+        if (!validators.TryGetValue(modelType.TypeHandle, out validator))
+        {
+            var typeName = string.Format("{0}.{1}Validator", modelType.Namespace, modelType.Name);
+            var type = modelType.Assembly.GetType(typeName, true);
+            validators[modelType.TypeHandle] = validator = (IValidator)Activator.CreateInstance(type);
+        }
+
+        return validator;
+    }
+
     void Validate(object sender, PropertyChangedEventArgs e)
     {
-
         validationResult = validator.Validate(target);
         foreach (var error in validationResult.Errors)
         {
