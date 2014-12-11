ebb3159cf825dc1ea471e933594d2667eba8c197
Simon Cropp <simon.cropp@gmail.com>
2014-12-11 20:15:02 +1100

    version

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyTitle("Validar")]
 [assembly: AssemblyProduct("Validar")]
-[assembly: AssemblyVersion("1.4.6")]
-[assembly: AssemblyFileVersion("1.4.6")]
+[assembly: AssemblyVersion("1.4.7")]
+[assembly: AssemblyFileVersion("1.4.7")]
