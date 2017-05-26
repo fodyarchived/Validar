d48a25662cc88a6f4a9563aa8ee1821f20fea610
Simon Cropp <simon.cropp@gmail.com>
2017-05-26 12:53:43 +1000

    Update CommonAssemblyInfo.cs

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyTitle("Validar")]
 [assembly: AssemblyProduct("Validar")]
-[assembly: AssemblyVersion("1.5.0")]
-[assembly: AssemblyFileVersion("1.5.0")]
+[assembly: AssemblyVersion("1.5.1")]
+[assembly: AssemblyFileVersion("1.5.1")]
